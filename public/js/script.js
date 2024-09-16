66bc59669809d94b6a6b1214146e30bcef31507f
durgesh kumar <durgeshraj1900069@gmail.com>
2024-09-17 00:30:20 +0530

    first commit
    first coomit

---
+++ public/js/script.js
@@ -0,0 +1,46 @@
+
+const socket = io(); 
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        socket.emit("send-location", { latitude, longitude });
+    }, (error) => {
+        console.error(error);
+    }, {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+    });
+}
+
+// Ensure you have included Leaflet CSS and JS files
+const map = L.map("map").setView([0, 0], 16);
+
+// Check if the tile layer URL is correct
+L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "Durgesh Coding School"
+}).addTo(map);
+
+const markers = {};
+
+socket.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
+    map.setView([latitude, longitude]);
+
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+      
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+});
+socket.ob("user-disconnect",(id)=>{
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+})
+
+
+
